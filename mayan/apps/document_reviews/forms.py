2f436bf87269503e9ba1c864630913862aa6d145
mpannala12 <mpannala@andrew.cmu.edu>
2021-09-26 09:59:17 -0400

    Created initial version of forms.py

--- mayan/apps/document_reviews/forms.py
+++ mayan/apps/document_reviews/forms.py
@@ -1,16 +1,18 @@
 from django import forms
 
-from mayan.apps.views.forms import DetailForm
-
 from .models import Review
 
 
-class DocumentReviewDetailForm(DetailForm):
+class DocumentReviewDetailForm(forms.Form):
 
-    class Meta:
-        fields = ('addlcomments',)
-        extra_fields = (
-            {'field': 'submit_date', 'widget': forms.widgets.DateTimeInput},
-            {'field': 'user'},
-        )
-        model = Review
+    # basic information
+    firstName = forms.CharField(label='First name', required=True)
+    lastName = forms.CharField(label='First name', required=True)
+    gradYear = forms.IntegerField(label='Graduation year', required=True)
+    fieldOfStudy = forms.CharField(label='Field of Study', required=True)
+
+    # scales for ratings (1-10)
+    gpaScale = forms.IntegerField(label='GPA rating', widget=forms.NumberInput(attrs={'type':'range', 'step': '1', 'min': '1', 'max': '10', 'id':'gpa_scale'}), required=True)
+    techinicalScale = forms.IntegerField(label='Technical skills', widget=forms.NumberInput(attrs={'type':'range', 'step': '1', 'min': '1', 'max': '10', 'id':'gpa_scale'}), required=True)
+    communicationScale = forms.IntegerField(label='Communication skills', widget=forms.NumberInput(attrs={'type':'range', 'step': '1', 'min': '1', 'max': '10', 'id':'gpa_scale'}), required=True)
+    experienceScale = forms.IntegerField(label='Experience', widget=forms.NumberInput(attrs={'type':'range', 'step': '1', 'min': '1', 'max': '10', 'id':'gpa_scale'}), required=True)
