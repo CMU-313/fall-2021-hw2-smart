6a738da38786cdbd24b04cea6aa40d0b95ba4c72
Terrance Lee <texeirareborn@macbook-pro-105.wifi.local.cmu.edu>
2021-09-27 13:01:04 -0400

    created defaults for migratiosn

--- mayan/apps/document_reviews/migrations/0002_additionalFields.py
+++ mayan/apps/document_reviews/migrations/0002_additionalFields.py
@@ -3,7 +3,7 @@ from django.db import models, migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('documennt_reviews', '0001_initial'),
+        ('document_reviews', '0001_initial'),
     ]
 
     operations = [
@@ -11,56 +11,56 @@ class Migration(migrations.Migration):
             model_name='Review',
             name='firstName',
             field=models.CharField(
-                verbose_name='First Name'
+                max_length=48, verbose_name='First Name', default="fname"
             ),
         ),
         migrations.AddField(
             model_name='Review',
             name='lastName',
             field=models.CharField(
-                verbose_name='lastName'
+                max_length=48, verbose_name='Last Name', default="lname"
             ),
         ),
         migrations.AddField(
             model_name='Review',
             name='gradYear',
             field=models.CharField(
-                verbose_name='Graduation Year'
+                max_length=48, verbose_name='Graduation Year', default="gyear"
             ),
         ),
         migrations.AddField(
             model_name='Review',
             name='fieldOfStudy',
             field=models.CharField(
-                verbose_name='Field of Study'
+                max_length=48, verbose_name='Field of Study', default="None"
             ),
         ),
         migrations.AddField(
             model_name='Review',
             name='gpaScale',
             field=models.IntegerField(
-                verbose_name='GPA: Rate 1-10'
+                verbose_name='GPA: Rate 1-10', default=0
             ),
         ),
         migrations.AddField(
             model_name='Review',
-            name='techinicalScale',
+            name='technicalScale',
             field=models.IntegerField(
-                verbose_name='Technical Skills: Rate 1-10'
+                verbose_name='Technical Skills: Rate 1-10', default=0
             ),
         ),
         migrations.AddField(
             model_name='Review',
             name='communicationScale',
             field=models.IntegerField(
-                verbose_name='Communication Skills: Rate 1-10'
+                verbose_name='Communication Skills: Rate 1-10', default=0
             ),
         ),
         migrations.AddField(
             model_name='Review',
             name='experienceScale',
             field=models.IntegerField(
-                verbose_name='Experience: Rate 1-10'
+                verbose_name='Experience: Rate 1-10', default=0
             ),
         ),
     ]
